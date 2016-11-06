09b2debf0278b357f274e6cbe1713f3c6099e9ab
masaki_kuwako <m.kuwako0702@gmail.com>
2016-11-06 23:47:33 +0900

    1+2ができない...

--- hensu.py
+++ hensu.py
@@ -1,15 +1,9 @@
 import tensorflow as tf
 
-var1 = tf.Variable(0)
-const2 = tf.constant(3)
+x = tf.constant(1, name='x')
+y = tf.Variable(x + 2, name='y')
 
-add_op = tf.add(var1, const2)
-update_var1 = tf.assign(var1, add_op)
-
-mul_op = tf.mul(add_op, update_var1)
-
-with tf.Session() as sess:
-    sess.run(tf.initialize_local_variables())
-
-    print(sess.run([mul_op]))
-    print(sess.run([mul_op]))
+model = tf.initialize_local_variables()
+with tf.Session() as session:
+    session.run(model)
+    print(session.run(y))
